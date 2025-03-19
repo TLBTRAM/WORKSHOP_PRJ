fc529f8d519c8b5005dfb04e3e421379ac1cf630
TLBTRAM <thailambaotram06@gmail.com>
2025-03-19 13:35:25 +0700

    đ

--- Workshop_2/SQL/Questions.sql
+++ Workshop_2/SQL/Questions.sql
@@ -21,10 +21,10 @@ CREATE TABLE tblQuestions (
 GO
 
 INSERT INTO tblQuestions (question_id, exam_id, question_text, option_a, option_b, option_c, option_d, correct_option) VALUES
-(1, 1, N'Số nào là số nguyên tố?', '4', '6', '7', '9', 'C'), -- SỐ nào là số nguyên tố: 7 là số nguyên tố nên đáp án C
-(2, 1, N'Kết quả của 5 + 3 × 2 là?', '16', '11', '10', '13', 'B'), -- 
-(3, 2, N'Vật lý: Đơn vị đo lực là gì?', 'Newton', 'Joule', 'Watt', 'Pascal', 'A'),
-(4, 3, N'Nguyên tố nào có ký hiệu hóa học là O?', N'Vàng', N'Oxi', N'Bạc', N'Sắt', 'B'),
+(1, 1, N'Which number is prime?', '4', '6', '7', '9', 'C'), -- SỐ nào là số nguyên tố: 7 là số nguyên tố nên đáp án C
+(2, 1, N'Result: 5 + 3 × 2=', '16', '11', '10', '13', 'B'), -- 
+(3, 2, N'What is a force measurement unit?', 'Newton', 'Joule', 'Watt', 'Pascal', 'A'),
+(4, 3, N'Which element has a chemical Oxygen?', 'Au', 'Oxi', 'Ag', 'Fe', 'B'),
 (5, 4, N'Which is the synonym of "happy"?', 'Sad', 'Joyful', 'Angry', 'Bored', 'B'),
-(6, 5, N'[Hello]?', N'Yêu bạn', N'Tôi khỏe', N'Xin chào', N'Tạm biệt', 'C');
+(6, 5, N'[Hello] Find the Synonymous?', N'I love you', N'IDK', N'Hi', N'Bye', 'C');
 GO
